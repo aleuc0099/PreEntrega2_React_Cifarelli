04406e2f722268df322530e5cf5c52fed2d40c9a
aleuc <aleuc0099@gmail.com>
2024-07-30 22:34:56 -0300

    Changes to ItemCount

--- src/components/itemCount/ItemCountContent.jsx
+++ src/components/itemCount/ItemCountContent.jsx
@@ -1,4 +1,15 @@
-const ItemCountContent = (counter, substCounter, addCounter, addToCart) => {
+/* eslint-disable react/prop-types */
+const ItemCountContent = ({counter, setCounter, addToCart}) => {
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const substCounter = () => {
+    if (counter > 1) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div>
       <button onClick={substCounter}>-</button>
