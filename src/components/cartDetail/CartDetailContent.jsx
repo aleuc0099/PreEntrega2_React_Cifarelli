fb94887d2ba6ef99469f58a7bdc8925ac420da67
aleuc <aleuc0099@gmail.com>
2024-07-31 01:27:43 -0300

    Changes to Styles

--- src/components/cartDetail/CartDetailContent.jsx
+++ src/components/cartDetail/CartDetailContent.jsx
@@ -2,7 +2,7 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
-import "../../styles/Cart_detail/Cart_detail.css"
+import "../../styles/Cart_detail/Cart_detail.css";
 
 const CartDetailContent = ({ cart }) => {
   const { totalPrice, eraseProductById } = useContext(CartContext);
@@ -17,6 +17,9 @@ const CartDetailContent = ({ cart }) => {
   }
   return (
     <div id="main_cart_box">
+      <div id="cart_header_box">
+        <h1 id="cart_header_txt">Cart</h1>
+      </div>
       {cart.map((cartProduct) => (
         <div key={cartProduct.id} id="cart_box">
           <p id="product_title">{cartProduct.title}</p>
@@ -25,12 +28,17 @@ const CartDetailContent = ({ cart }) => {
             src={cartProduct.image}
             alt={cartProduct.title}
           />
-          <p id="unitary_price_p">Price ${cartProduct.price * cartProduct.quantity}</p>
-          <button onClick={() => eraseProductById(cartProduct.id)}>
+          <p id="unitary_price_p">
+            Price ${cartProduct.price * cartProduct.quantity}
+          </p>
+          <button id="eraser_btn" onClick={() => eraseProductById(cartProduct.id)}>
             Erase Product
           </button>
         </div>
       ))}
+      <div id="empty_cart_btn_box">
+      <button id="empty_cart_btn">Empty Cart</button>
+      </div>
       <p id="total_price_p">Total Price: ${price}</p>
     </div>
   );
