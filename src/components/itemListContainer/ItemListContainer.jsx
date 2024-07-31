6a2fd294b4f58843689080b3f727debb0cfc2ca5
aleuc <aleuc0099@gmail.com>
2024-07-31 01:54:26 -0300

    Changes to Styles

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import ItemListContainerContent from "./ItemListContainerContent";
 import { my_products } from "../../assets/data/myproducts";
-import "../../styles/Item_list/Item_list.css"
+import "../../styles/Item_list/Item_list.css";
 
 const ItemListContainer = () => {
   const [products, set_products] = useState([]);
@@ -10,14 +10,14 @@ const ItemListContainer = () => {
   const { category } = useParams();
 
   useEffect(() => {
-        if (category) {
-          const filtered_products = my_products.filter(
-            (product) => product.category === category
-          );
-          set_products(filtered_products);
-        } else {
-          set_products(my_products);
-        }
+    if (category) {
+      const filtered_products = my_products.filter(
+        (product) => product.category === category
+      );
+      set_products(filtered_products);
+    } else {
+      set_products(my_products);
+    }
   }, [category]);
 
   return <ItemListContainerContent products={products} />;
