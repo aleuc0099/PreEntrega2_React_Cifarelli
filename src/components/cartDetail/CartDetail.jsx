93a923c5b3c15b0d307a20c48db747c31eec1555
aleuc <aleuc0099@gmail.com>
2024-07-31 00:54:30 -0300

    Changes to Styles

--- src/components/cartDetail/CartDetail.jsx
+++ src/components/cartDetail/CartDetail.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
 import CartDetailContent from "./CartDetailContent";
+import { CartContext } from "../../context/CartContext";
 
 const CartDetail = () => {
-  return <CartDetailContent />;
+  const { cart, eraseProducById } = useContext(CartContext);
+  return (
+    <CartDetailContent
+      cart={cart}
+      eraseProducById={eraseProducById}
+    />
+  );
 };
 
 export default CartDetail;
--- src/components/cartDetail/CartDetailContent.jsx
+++ src/components/cartDetail/CartDetailContent.jsx
@@ -1,7 +1,37 @@
-const CartDetailContent = () => {
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
+import "../../styles/Cart_detail/Cart_detail.css"
+
+const CartDetailContent = ({ cart }) => {
+  const { totalPrice, eraseProductById } = useContext(CartContext);
+  const price = totalPrice();
+  if (cart.lenght < 1) {
+    return (
+      <div>
+        <h1>The Cart is Empty!</h1>
+        <Link to={"/"}>Shop Now!</Link>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Cart</h1>
+    <div id="main_cart_box">
+      {cart.map((cartProduct) => (
+        <div key={cartProduct.id} id="cart_box">
+          <p id="product_title">{cartProduct.title}</p>
+          <img
+            id="product_image"
+            src={cartProduct.image}
+            alt={cartProduct.title}
+          />
+          <p id="unitary_price_p">Price ${cartProduct.price * cartProduct.quantity}</p>
+          <button onClick={() => eraseProductById(cartProduct.id)}>
+            Erase Product
+          </button>
+        </div>
+      ))}
+      <p id="total_price_p">Total Price: ${price}</p>
     </div>
   );
 };
