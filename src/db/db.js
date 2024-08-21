06bdc95ec56147deb53e0d0b154f30eefda14b33
aleuc <aleuc0099@gmail.com>
2024-08-21 14:43:54 -0300

    db created

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCkXPHTDsTQTS_fnoledavE5km6fBd6Vz0",
   authDomain: "alejo-foods.firebaseapp.com",
   projectId: "alejo-foods",
   storageBucket: "alejo-foods.appspot.com",
   messagingSenderId: "800689593567",
-  appId: "1:800689593567:web:ea24511d75ee961fb85d6a"
+  appId: "1:800689593567:web:ea24511d75ee961fb85d6a",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
